a70881d00f3130ab789683d5f56d74215fe19c54
David Mann <manndavidj@gmail.com>
2018-04-16 15:23:28 -0400

    Create build.sh
    Build script for staged Multus Openshift node build

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+echo Building badenglishihave/origin-node-multus:build
+
+docker build --build-arg -t badenglishihave/origin-node-multus:build \
+   . -f Dockerfile.build
+
+docker container create --name extract badenglishihave/origin-node-multus:build  
+docker container cp extract:/usr/src/multus-cni/bin/multus ./opt/cni/bin/
+docker container cp extract:/usr/src/plugins/bin/* ./opt/cni/bin/
+docker container rm -f extract
+
+echo Building badenglishihave/origin-node-multus:latest
+
+docker build --no-cache -t badenglishihave/origin-node-multus:latest .
+rm -rf ./opt
