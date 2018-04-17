65e1a2dda5678495c5edf5e82e6340fe248b5143
root <root@osmaster-1.mydomain.com>
2018-04-17 09:30:02 -0400

    fixed wildcard file copy in build script

--- build.sh
+++ build.sh
@@ -1,15 +1,19 @@
 #!/bin/sh
-echo Building badenglishihave/origin-node-multus:build
+echo Building manndavidj/origin-node-multus:build
 
-docker build -t badenglishihave/origin-node-multus:build -f Dockerfile.build .
+docker build -t manndavidj/origin-node-multus:build -f Dockerfile.build .
 
 mkdir -p ./opt/cni
-docker container create --name extract badenglishihave/origin-node-multus:build  
+docker container create --name extract manndavidj/origin-node-multus:build  
 docker container cp extract:/usr/src/multus-cni/bin/multus ./opt/cni/bin/
-docker container cp extract:/usr/src/plugins/bin/* ./opt/cni/bin/
+#docker container cp extract:/usr/src/plugins/bin/* ./opt/cni/bin/
+PLUGINS=$(docker run -i -a stdout manndavidj/origin-node-multus /bin/bash -c "find /usr/src/plugins/bin -type f")
+for plugin in $PLUGINS; do
+	docker cp extract:$plugin ./opt/cni/bin || true
+done
 docker container rm -f extract
 
-echo Building badenglishihave/origin-node-multus:latest
+echo Building manndavidj/origin-node-multus:latest
 
-docker build --no-cache -t badenglishihave/origin-node-multus:latest .
+docker build --no-cache -t manndavidj/origin-node-multus:latest .
 rm -rf ./opt
